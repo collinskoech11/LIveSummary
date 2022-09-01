1c9349de9ee0c3c44fc3a282f3ff7e5c3c87cb9d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-01 12:54:17 +0300

    Create dbStructure.php

---
+++ dbStructure.php
@@ -0,0 +1,8 @@
+Users:
+'id','username','role','Course','email','password'
+Units:
+'unit_id','unit_name','course_name','faculty'
+Courses:
+'course_id','course_name','faculty'
+Articles:
+'article_id','user_id','username','article_title','article_body','created_at','unit_title','course')
